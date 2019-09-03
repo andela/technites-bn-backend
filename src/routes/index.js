e2f044475d92efd6ddd6fc39598d697d865aac15
amily kassim <amilykassim02@gmail.com>
2019-09-03 18:17:26 +0200

    chore(api-documentation): setup api-documentation [Start #168049993]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('../../swagger.json');
 
 router.use("/api", require("./api"));
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 module.exports = router;
