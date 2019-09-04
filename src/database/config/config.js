70f8e34e78a9d596fc37b4ab6af8bdcd3a6de6fc
Mucyo Fred <mucyomiller@gmail.com>
2019-09-04 16:21:02 +0200

    chore(setup-sequelize): adds database configurations  adds feedback from TTL [Start #168049985]

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,8 +6,8 @@ module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: `${process.env.DB_NAME}_development`,
-    host: '127.0.0.1',
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     operatorsAliases: false,
     logging: true,
@@ -15,19 +15,15 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: `${process.env.DB_NAME}_test`,
-    host: '127.0.0.1',
+    database: process.env.TEST_DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     operatorsAliases: false,
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: `${process.env.DB_NAME}_production`,
-    host: '127.0.0.1',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    operatorsAliases: false,
     logging: false,
   }
 };
