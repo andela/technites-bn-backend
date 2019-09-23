e104b76ed61cf6bc6b909b3b3f486a5c5bb069c3
Didas Mbalanya <didas.mbalanya@Didass-MacBook-Pro.local>
2019-09-23 11:35:21 +0200

    feat(post-comments): add post comments feature for user or manager on a request
    [Start #168050013]

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,9 +4,7 @@ import UserController from '../controllers/UserController';
 import UserAuthentication from '../middlewares/UserAuthentication';
 import Validation from '../validation/Validations';
 import RequestController from '../controllers/RequestController';
-import CommentController from '../controllers/CommentController';
-import { commentdata, validator } from '../validation/UserValidation';
-
+import { checkIsInt, validator } from '../validation/UserValidation';
 
 const router = new Router();
 
@@ -15,30 +13,24 @@ const connection = connect();
 const { verifyToken } = UserAuthentication;
 const {
   editProfile, viewSingleProfile, viewAllProfiles,
-  viewProfilesByCompany, enableOrDisableEmailNotifications,
+  viewProfilesByCompany, enableOrDisableEmailNotifications, getUserTrips
 } = UserController;
 
 const { getRequests } = RequestController;
 
-const {
-  createComment, getUserRequestComments, editRequestComment, deleteComment
-} = CommentController;
 const { updateProfileValidator } = Validation;
 
 // profiles
 router.patch('/editprofile', verifyToken, connection, updateProfileValidator, editProfile);
-router.get('/user/:id', viewSingleProfile);
-router.get('/users/all', viewAllProfiles);
-router.get('/users/company/:company', viewProfilesByCompany);
-router.post('/users/notifications', [verifyToken], enableOrDisableEmailNotifications);
+router.post('/notifications', [verifyToken], enableOrDisableEmailNotifications);
+router.get('/:id', viewSingleProfile);
+router.get('/', viewAllProfiles);
+router.get('/company/:company', viewProfilesByCompany);
 
 // get requests
-router.get('/users/:id/requests', verifyToken, getRequests);
+router.get('/:id/requests', verifyToken, getRequests);
 
-// comments
-router.post('/requests/:request_id/comments', commentdata, validator, verifyToken, createComment);
-router.get('/requests/:request_id/comments', verifyToken, getUserRequestComments);
-router.patch('/requests/:request_id/comments/:comment_id', commentdata, validator, verifyToken, editRequestComment);
-router.delete('/requests/:request_id/comments/:comment_id', verifyToken, deleteComment);
+// get user trips
+router.get('/:id/trips?', checkIsInt, validator, verifyToken, getUserTrips);
 
 export default router;
